5c98a8c53fed98f83d8245e5c558b4814d89d016
gmr2048 <gmr2048@yahoo.com>
2025-05-24 13:35:35 -0400

    Update PiholeAggAdlistUpdate.sh
    New update method. No longer push to/pull from github.

--- PiholeAggAdlistUpdate.sh
+++ PiholeAggAdlistUpdate.sh
@@ -3,24 +3,23 @@
 #C:\Users\Gary.DESKTOP-J47QPHF\Downloads\pihole-lists
 #cron job to do this daily
 
-echo "curl download"
 #curl download all the individual lists
-	curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts --output /home/gmr2048/Documents/PiHole/StevenBlackHosts.txt
-	curl https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt --output /home/gmr2048/Documents/PiHole/simple_tracking.txt
-	curl https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt --output /home/gmr2048/Documents/PiHole/simple_ad.txt
-	curl https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt --output /home/gmr2048/Documents/PiHole/ads-and-tracking-extended.txt
-	curl https://v.firebog.net/hosts/static/w3kbl.txt --output /home/gmr2048/Documents/PiHole/w3kbl.txt
-	curl https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt --output /home/gmr2048/Documents/PiHole/AntiMalwareHosts.txt
-	curl https://v.firebog.net/hosts/RPiList-Malware.txt --output /home/gmr2048/Documents/PiHole/RPiList-Malware.txt
-	curl https://v.firebog.net/hosts/RPiList-Phishing.txt --output /home/gmr2048/Documents/PiHole/RPiList-Phishing.txt
-	curl https://v.firebog.net/hosts/Admiral.txt --output /home/gmr2048/Documents/PiHole/Admiral.txt
-	curl https://v.firebog.net/hosts/Prigent-Malware.txt --output /home/gmr2048/Documents/PiHole/Prigent-Malware.txt
+        curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts --output /home/gmr2048/Documents/PiHole/outfiles/StevenBlackHosts.txt
+        curl https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt --output /home/gmr2048/Documents/PiHole/outfiles/simple_tracking.txt
+        curl https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt --output /home/gmr2048/Documents/PiHole/outfiles/simple_ad.txt
+        curl https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt --output /home/gmr2048/Documents/PiHole/outfiles/ads-and-tracking-extended.txt
+        curl https://v.firebog.net/hosts/static/w3kbl.txt --output /home/gmr2048/Documents/PiHole/outfiles/w3kbl.txt
+        curl https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt --output /home/gmr2048/Documents/PiHole/outfiles/AntiMalwareHosts.txt
+        curl https://v.firebog.net/hosts/RPiList-Malware.txt --output /home/gmr2048/Documents/PiHole/outfiles/RPiList-Malware.txt
+        curl https://v.firebog.net/hosts/RPiList-Phishing.txt --output /home/gmr2048/Documents/PiHole/outfiles/RPiList-Phishing.txt
+        curl https://v.firebog.net/hosts/Admiral.txt --output /home/gmr2048/Documents/PiHole//outfiles/Admiral.txt
+        curl https://v.firebog.net/hosts/Prigent-Malware.txt --output /home/gmr2048/Documents/PiHole/outfiles/Prigent-Malware.txt
+        curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.plus.txt --output /home/gmr2048/Documents/PiHole/outfiles/hagezi-pro.plus.txt
+        curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt --output /home/gmr2048/Documents/PiHole/outfiles/hagezi-tif.txt
+        curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.tiktok.txt  --output /home/gmr2048/Documents/PiHole/outfiles/hagezi-tiktok.txt
 
-echo "combine lists"
-#combine lists, remove dupes and blank lines, remove all comment lines, split into 1.4M line files
-	cat /home/gmr2048/Documents/PiHole/*.txt | grep -v '^\s*$' | sort -u | sed '/^#/d' | split -l400000 - outfile_
+#combine lists, remove dupes and blank lines, remove all comment lines, (not doing: split into 400k line files)
+        cat /home/gmr2048/Documents/PiHole/outfiles/*.txt | grep -v '^\s*$' | sort -u | sed '/^#/d' | split -l9999999999 - /home/gmr2048/Documents/PiHole/outfiles/outfile_
 
-echo "clean up"
 #clean up source text files
-	rm /home/gmr2048/Documents/PiHole/*.txt
- 
+        rm /home/gmr2048/Documents/PiHole/outfiles/*.txt
